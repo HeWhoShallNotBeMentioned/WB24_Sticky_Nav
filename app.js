f1ed1508925f51c7d2e70aa1b9a010967ab18594
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-10 19:51:57 -0800

    code for smoothing out the loss of the nav bar in the dom

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const topOfNav = nav.offsetTop;
 function fixNav() {
   //console.log(topOfNav);
   if(window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
       document.body.classList.remove('fixed-nav');
+      document.body.style.paddingTop = 0;
   }
 }
 
